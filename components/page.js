aa821343ff1731e4fb64360551638367d11a28c2
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-02-23 20:08:33 +0200

    Add GA

--- components/page.js
+++ components/page.js
@@ -1,5 +1,17 @@
 import Head from 'next/head';
 
+const GA_CODE = `
+(function (i, s, o, g, r, a, m) {
+i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)        
+}, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-70971437-1', 'auto');
+ga('send', 'pageview');
+`;
+
 export default ({ title, children }) => (
     <div>
         <Head>
@@ -9,6 +21,7 @@ export default ({ title, children }) => (
             <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" />
             <link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/heflipieckodmcppbnembejjmabajjjj" />
             <link rel="stylesheet" href="/static/style.css" type="text/css" />
+            <script dangerouslySetInnerHTML={{__html: GA_CODE}}></script>
         </Head>
         {children}
     </div>
