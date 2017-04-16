b328234d1429b850111f243e65da184b6af0681f
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-04-16 23:19:40 +0300

    Add privacy policy

--- pages/index.js
+++ pages/index.js
@@ -4,41 +4,6 @@ import LatestVersion from '../components/version';
 
 export default () => (
     <Page title="Buttercup">
-        <header>
-            <nav className="nav has-shadow">
-                <div className="container">
-                    <div className="nav-left">
-                        <a className="nav-item">
-                            <img src="/static/img/logo.svg" alt="Buttercup Logo"/>
-                        </a>
-                    </div>
-
-                        <div className="nav-center">
-                            <a className="nav-item" href="https://github.com/buttercup-pw/buttercup" rel="noopener" target="_blank">
-                                <span className="icon">
-                                    <i className="fa fa-github"></i>
-                                </span>
-                            </a>
-                            <a className="nav-item" href="https://twitter.com/buttercup_pw" rel="noopener" target="_blank">
-                                <span className="icon">
-                                    <i className="fa fa-twitter"></i>
-                                </span>
-                            </a>
-                        </div>
-
-                        <div className="nav-right nav-menu">
-                            <a className="nav-item" href="/">Home</a>
-                            <a className="nav-item" href="https://chrome.google.com/webstore/detail/buttercup/heflipieckodmcppbnembejjmabajjjj?hl=en">Chrome Extension</a>
-
-                            <span className="nav-item">
-                                <a className="button is-primary" href="#download">
-                                    <span>Download</span>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-            </nav>
-        </header>
         <section className="hero is-primary">
             <div className="hero-body">
                 <div className="container has-text-centered">
